28b2f81b7dd3c6dd5902cedcdf6bc5abc91e1396
Harpreet Singh <hsingh9600@gmail.com>
2021-06-21 04:44:06 +1200

    move decodeInstruction()

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -4,6 +4,10 @@ type Cpu struct {
 	ram memory
 }
 
+func decodeInstruction(instruction uint16) (uint16, [3]uint16) {
+	return instruction >> 12, [3]uint16{instruction >> 8 & 15, instruction >> 4 & 15, instruction & 15}
+}
+
 func (c Cpu) executeInstruction(instruction uint16) {
 	opcode, vars := decodeInstruction(instruction)
 
--- cpu/decodeFunctions.go
+++
@@ -1,5 +0,0 @@
-package cpu
-
-func decodeInstruction(instruction uint16) (uint16, [3]uint16) {
-	return instruction >> 12, [3]uint16{instruction >> 8 & 15, instruction >> 4 & 15, instruction & 15}
-}
